8a4b9277f3f0f6a7cf63a5d6a27c1e96f18b1db0
Vuong_DZ <vuonga7k8lnc@gmail.com>
2020-12-02 19:15:07 +0700

    upload img

--- views/Shop/Blogs/sidebar.php
+++ views/Shop/Blogs/sidebar.php
@@ -8,31 +8,45 @@
                 </form>
             </div>
         </div>
-        <?php use webLazy\Core\URL;
-        use webLazy\Model\CategoryModel;
-        use webLazy\Model\NewsModel;
-        use webLazy\Model\TypeNewsModel;
+	    <?php use webLazy\Core\URL;
+	    use webLazy\Model\CategoryModel;
+	    use webLazy\Model\NewsModel;
+	    use webLazy\Model\TypeNewsModel;
 
-        $aCategory = CategoryModel::selectAll();
-        foreach ($aCategory as $value): ?>
-            <div class="li-blog-sidebar pt-25">
-                <h4 class="li-blog-sidebar-title"><?= $value[1] ?></h4>
-                <ul class="li-blog-archive">
-                    <?php $aTypeNews = TypeNewsModel::selectWithIdCategory($value[0]);
-                    foreach ($aTypeNews as $aValues): ?>
-                        <li><a href=""><?= $aValues[2] ?>
-                                (<?php echo NewsModel::selectCountNew($aValues[0]) ?>)</a></li>
-                    <?php endforeach; ?>
-                </ul>
+	    ?>
+        <div class="li-blog-sidebar pt-25">
+            <div class="sidebar-categores-box mt-sm-30 mt-xs-30">
+                <div class="sidebar-title">
+                    <h2>Menu Tin</h2>
+                </div>
+                <!-- category-sub-menu start -->
+                <div class="category-sub-menu">
+                    <ul>
+					    <?php
+					    $aCategory = CategoryModel::selectAll();
+					    foreach ($aCategory as $value): ?>
+                            <li class="has-sub"><a href=" "><?= $value[1] ?></a>
+                                <ul>
+								    <?php $aTypeNews = TypeNewsModel::selectWithIdCategory($value[0]);
+								    foreach ($aTypeNews as $aValues): ?>
+                                        <li><a href="#"><?= $aValues[2] ?> (<?php echo NewsModel::selectCountNew
+											    ($aValues[0]) ?>)</a></li>
+								    <?php endforeach; ?>
+                                </ul>
+                            </li>
+					    <?php endforeach; ?>
+                    </ul>
+                </div>
+                <!-- category-sub-menu end -->
             </div>
-        <?php endforeach; ?>
+        </div>
         <div class="li-blog-sidebar">
             <h4 class="li-blog-sidebar-title">Top 5 Bài Viết Được Xem Nhiều Nhất:</h4>
-            <?php foreach (NewsModel::popularPostCouview() as $value): ?>
+		    <?php foreach (NewsModel::popularPostCouview() as $value): ?>
                 <div class="li-recent-post pb-30">
                     <div class="li-recent-post-thumb" style="width: 100px;height: 120px; ">
                         <a href="<?= URL::uri('singerBlog') . '/' . $value[0] ?>">
-                            <?php $imgNav = json_decode($value[5], true); ?>
+						    <?php $imgNav = json_decode($value[5], true); ?>
                             <img style="width: 63px;height: 63px; margin-top: 20px;margin-left: 3px;" class="img-full"
                                  src="./assets/upload/News/<?= $imgNav[1] ?>"
                                  alt="Li's Product Image">
--- views/Shop/header.php
+++ views/Shop/header.php
@@ -48,6 +48,7 @@
     <script src="./assets/ajax.js"></script>
 </head>
 <body>
+<!-- Load Facebook SDK for JavaScript -->
 <!--[if lt IE 8]>
 <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 <![endif]-->
